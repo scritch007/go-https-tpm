f8c09bbb103ad5bbafb14ab83a53dfd70d5dc2ab
Benjamin Legrand <dev@legrand.ws>
2020-04-17 23:20:41 +0200

    [tpm2] Simplify library

--- pkg/tpm2/private-key-tpm2.go
+++ pkg/tpm2/private-key-tpm2.go
@@ -12,7 +12,6 @@ import (
 	"github.com/pkg/errors"
 
 	"io"
-	"runtime"
 	"sync"
 )
 
@@ -21,29 +20,18 @@ func LoadPrivateKeyFromTPM2(device string, handle tpmutil.Handle, password strin
 
 	w := &wrapperTPM2{
 		device:   device,
-		handle:   handle,
 		password: password,
 	}
-
-	pk, tpmClose, err := w.getPk()
-	defer tpmClose.Close()
-	if err != nil {
-		return nil, errors.Wrap(err, "TPM2 retrieve private key")
-	}
-
-	w.publicKey = pk.publicKey
-
-	return w, nil
+	return w, errors.Wrap(w.init(handle), "couldn't initialise key")
 }
 
 type wrapperTPM2 struct {
 	device    string
-	handle    tpmutil.Handle
+	handle    tpm2.ResourceContext
 	publicKey crypto.PublicKey
 	lock      sync.Mutex
 	password  string
 	other     crypto.Signer
-	called    int
 }
 
 func (w *wrapperTPM2) Public() crypto.PublicKey {
@@ -84,9 +72,9 @@ var hashToName = map[crypto.Hash]string{
 
 func (w *wrapperTPM2) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts) (signature []byte, err error) {
 	fmt.Println("Sign called")
-	pk, close, err := w.getPk()
+	dev, err := w.openTPM()
 	defer func() {
-		close.Close()
+		w.closeTPM(dev)
 	}()
 	if err != nil {
 		return nil, errors.Wrap(err, "Sign: retrieving private key")
@@ -109,7 +97,7 @@ func (w *wrapperTPM2) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts
 	//shaDigest := sha256.Sum256(digest)
 	shaDigest := digest
 
-	s, err := pk.tpm.Sign(pk.handle, tpm2.Digest(shaDigest[:]), &inScheme, nil, nil)
+	s, err := dev.Sign(w.handle, tpm2.Digest(shaDigest[:]), &inScheme, nil, nil)
 
 	if err != nil {
 		return nil, errors.Wrap(err, "sign failed")
@@ -131,74 +119,22 @@ func (w *wrapperTPM2) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts
 	return
 }
 
-type closeWrapper struct {
-	close func() error
-	dev   io.ReadWriter
-}
-
-func (c closeWrapper) Read(p []byte) (n int, err error) {
-	return c.dev.Read(p)
-}
-
-func (c closeWrapper) Write(p []byte) (n int, err error) {
-	return c.dev.Write(p)
-}
-
-func (c closeWrapper) Close() error {
-	return c.close()
-}
-
-func (w *wrapperTPM2) getPk() (pk *rsaPkey, cw closeWrapper, err error) {
-	w.lock.Lock()
-	_, file, no, ok := runtime.Caller(1)
-	if ok {
-		fmt.Printf("called from %s#%d\n", file, no)
+func (w *wrapperTPM2) init(handle tpmutil.Handle) error {
+	dev, err := w.openTPM()
+	if dev != nil {
+		defer w.closeTPM(dev)
 	}
-
-	cw = closeWrapper{close: func() error { return nil }}
-
-	var iow io.ReadWriteCloser
-
-	if w.device == "sim" {
-		iow, err = tpm2.OpenMssim("", 2321, 2322)
-	} else {
-		iow, err = tpm2.OpenTPMDevice(w.device)
-	}
-
 	if err != nil {
-		return nil, cw, errors.Wrap(err, "couldn't open tpm connection")
+		return err
 	}
-	dev, err := tpm2.NewTPMContext(iow)
+
+	pkey, err := dev.CreateResourceContextFromTPM(tpm2.Handle(uint32(handle)))
 	if err != nil {
-		return pk, cw, errors.Wrapf(err, "opening %s", w.device)
-	}
-
-	if err = dev.Startup(tpm2.StartupClear); err != nil {
-		if !strings.Contains(err.Error(), "TPM_RC_INITIALIZE") {
-			return nil, cw, errors.Wrap(err, "startup error")
-		}
-
-	}
-
-	cw = closeWrapper{
-		dev: iow,
-		close: func() error {
-			fmt.Println("TPM2 closed")
-			dev.Shutdown(tpm2.StartupClear)
-			err := dev.Close()
-			iow.Close()
-			w.lock.Unlock()
-			return errors.Wrap(err, "error closing")
-		},
-	}
-
-	pkey, err := dev.CreateResourceContextFromTPM(tpm2.Handle(uint32(w.handle)))
-	if err != nil {
-		return nil, cw, errors.Wrap(err, "couldn't get resource")
+		return errors.Wrap(err, "couldn't get resource")
 	}
 	pub, _, _, err := dev.ReadPublic(pkey)
 	if err != nil {
-		return nil, cw, errors.Wrap(err, "couldn't get public key")
+		return errors.Wrap(err, "couldn't get public key")
 	}
 
 	exp := int(pub.Params.RSADetail().Exponent)
@@ -206,17 +142,40 @@ func (w *wrapperTPM2) getPk() (pk *rsaPkey, cw closeWrapper, err error) {
 		exp = tpm2.DefaultRSAExponent
 	}
 	pubKey := &rsa.PublicKey{N: new(big.Int).SetBytes(pub.Unique.RSA()), E: exp}
-	pk = &rsaPkey{
-		publicKey: pubKey,
-		handle:    pkey,
-		tpm:       dev,
-	}
 
-	return pk, cw, nil
+	w.publicKey = pubKey
+	w.handle = pkey
+
+	return nil
 }
 
-type rsaPkey struct {
-	publicKey crypto.PublicKey
-	handle    tpm2.ResourceContext
-	tpm       *tpm2.TPMContext
+func (w *wrapperTPM2) openTPM() (*tpm2.TPMContext, error) {
+	w.lock.Lock()
+	var iow io.ReadWriteCloser
+	var err error
+	if w.device == "sim" {
+		iow, err = tpm2.OpenMssim("", 2321, 2322)
+	} else {
+		iow, err = tpm2.OpenTPMDevice(w.device)
+	}
+
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't open tpm connection")
+	}
+	dev, err := tpm2.NewTPMContext(iow)
+	if err != nil {
+		return nil, errors.Wrapf(err, "opening %s", w.device)
+	}
+
+	if err = dev.Startup(tpm2.StartupClear); err != nil {
+		if !strings.Contains(err.Error(), "TPM_RC_INITIALIZE") {
+			return nil, errors.Wrap(err, "startup error")
+		}
+	}
+	return dev, nil
+}
+
+func (w *wrapperTPM2) closeTPM(dev *tpm2.TPMContext) {
+	dev.Close()
+	w.lock.Unlock()
 }
